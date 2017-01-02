6606426bda6e6a6331c9b0d223e2d579a158f0d3
LoPCY7 <paris-messios@hotmail.com>
2017-01-02 16:58:01 +0200

    Added basic Reviews, Movies and Categories MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  resources :reviews
+
+  get 'movies/index'
+
   resources :posts
   root 'posts#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231122035) do
+ActiveRecord::Schema.define(version: 20170102143137) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
