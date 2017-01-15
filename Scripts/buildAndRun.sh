45aed13724149385fd78613a16902be3d40dc25e
LoPCY7 <paris-messios@hotmail.com>
2017-01-15 19:51:32 +0000

    Updated build and run script

--- Scripts/buildAndRun.sh
+++ Scripts/buildAndRun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-cd ..
+cd ~/pFlix/
 sed -i 's/STRIPE_PUBLISHABLE_KEY=!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/STRIPE_PUBLISHABLE_KEY=pk_test_tucThm6jftvGdjRfkBYhw7S3/g' docker-compose.yml
 sed -i 's/STRIPE_SECRET_KEY=!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/STRIPE_SECRET_KEY=sk_test_3Rd8y70RSmynFztLExEOksoz/g' docker-compose.yml
 docker-compose build
+docker-compose run app db:create
+docker-compose run app db:migrate
 docker-compose up
